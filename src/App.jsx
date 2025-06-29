0dbb22651e5f430998c3d0cf17206d1c590089b4
LindaTancini <lindatancini95@gmail.com>
2025-06-29 10:40:18 +0200

    feat: create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import ProductCard from "./components/ProductCard";
+
+function App() {
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,9 @@
+function ProductCard() {
+  return (
+    <div>
+      <h1>Card del prodotto</h1>
+    </div>
+  );
+}
+
+export default ProductCard;
