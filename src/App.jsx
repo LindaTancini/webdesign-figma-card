a360fbca184e7e57ccc9b50aba344d4c71bff715
LindaTancini <lindatancini95@gmail.com>
2025-06-29 11:29:25 +0200

    feat: add className on div

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./components/ProductCard";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <ProductCard />
     </div>
   );
