c7443d543bb959bec51c03f7794ab46e504f084d
LindaTancini <lindatancini95@gmail.com>
2025-06-29 10:55:02 +0200

    feat: add html structure

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+const size = ["300g", "2kg", "7kg"];
 function ProductCard() {
+  const [selectSize, setSelectSize] = useState("300g");
   return (
     <div>
-      <h1>Card del prodotto</h1>
+      <img src="" alt="Croccantini Whiskas" />
+      <div>
+        <h3>Whiskas</h3>
+        <p>Croccantini Junior</p>
+        <p>Alimento secco completo per cuccioli al pollo</p>
+        <hr />
+        <p>Taglia</p>
+        <div>
+          {size.map((e) => (
+            <button key={size}>{size}</button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
